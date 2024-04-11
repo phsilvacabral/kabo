0263db7b5cd1299ddf137c1c38aaa2114043fba8
Daniel Musse <127629791+iYoNuttxD@users.noreply.github.com>
2024-04-11 10:39:40 -0300

    Sistema de Login com PHP
    Pagina de erro e inicio ainda n foram atualizadas.

---
+++ admin/login/login_php.php
@@ -0,0 +1,33 @@
+<?php
+    include("connection.php");
+
+    $login = $_POST["txtLogin"];
+    $password = $_POST["txtPassword"];
+
+    $sql = "SELECT Cod_Usuario, Senha FROM Usuario WHERE Email = '$login'";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            if ($row["Senha"] == $password) {
+                session_start();
+                $_SESSION["Cod_Usuario"] = $row["Cod_Usuario"];
+                header("Location: /kabo/index.php");
+            } else {
+?>
+<script>
+    alert("Senha incorreta");
+    history.go(-1);
+</script>
+<?php
+            }
+        }
+    }
+    else {
+?>
+<script>
+    alert("Login incorreto");
+    history.go(-1);
+</script>
+<?php
+    }
+?>
