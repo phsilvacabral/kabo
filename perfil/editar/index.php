9d9a37b8726d3a8934df3b62a7973d27a664e4b4
Pedro Cabral <126737207+phsilvacabral@users.noreply.github.com>
2024-04-15 20:02:44 -0300

    Add files via upload

--- perfil/editar/index.php
+++ perfil/editar/index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=1.0, user-scalable=no">
     <link rel="stylesheet" href="style.css">
-    <link rel="shortcut icon" href="../../img_principais/icon_logo.png" type="image/x-icon">
+    <link rel="shortcut icon" href="../../img/logo_neon.png" type="image/x-icon">
     <title>Editar perfil</title>
 </head>
 
@@ -15,7 +15,7 @@
     session_start();
 
     if (!isset($_SESSION["Cod_Cliente"])) {
-        header("Location: /123folhas/");
+        header("Location: /kabo/");
         exit();
     }
 
@@ -56,7 +56,7 @@
             <span id="X"><a href="../">&times;</a></span>
 
             <div class="alinharelementos">
-                <img src="../../img_principais/logo_login.png" alt="" id="logo">
+                <img src="../../img/logo_neon.png" alt="logo" id="logo">
             </div>
 
             <div class="alinharelementosescrita">
@@ -67,38 +67,29 @@
             <form id="form1" name="form1" method="post" action="edit_php.php" onsubmit="return verificar()">
                 <div class="espacodentrobox">
 
-                    <input type="text" name="txtNome" maxlength="100" id="primeiroinput" placeholder="Nome" class="campocheio" value="<?php echo $Nome ?>" required>
+                    <input type="text" name="txtNome" value="" maxlength="100" id="primeiroinput" placeholder="Nome" class="campocheio" required>
 
-                    <input type="text" placeholder="CEP" id="CEP" name="txtCEP" oninput="this.value = maskCEP(this.value)" maxlength="9" class="campocheio" value="<?php echo $CEP ?>" required>
+                    <input type="text" placeholder="CPF" id="cpf" name="txtCPF" value="" oninput="this.value = maskCPF(this.value)" maxlength="14" class="campomedio" required>
+                    <select name="selectGenero" id="genero" class="campomedio" required>
+                        <option value="genero" disabled selected>Gênero</option>
+                        <option value="M">Masculino</option>
+                        <option value="F">Feminino</option>
+                        <option value="O">Outro</option>
+                    </select>
 
-                    <input type="email" name="email" id="email" maxlength="100" placeholder="E-mail" class="campocheio" value="<?php echo $Email ?>" required>
+                    <input type="date" placeholder="Data de nascimento" id="nascimento" name="dateData_Nasc" oninput="this.value = maskData(this.value)" class="campomedio" required>
+                    <input type="text" placeholder="CEP" id="CEP" name="txtCEP" value="" oninput="this.value = maskCEP(this.value); buscarCEP(this.value);" maxlength="9" class="campomedio" required>
 
-                    <input type="password" name="txtSenha" id="senha" maxlength="20" placeholder="Senha" class="campocheio" value="<?php echo $Senha_login ?>" required>
+                    <input type="text" placeholder="Logradouro" id="logradouro" name="txtLogradouro" value="" class="campocheio" maxlength="150" required>
+                    <input type="text" placeholder="Bairro" id="bairro" name="txtBairro" value="" class="campomedio" maxlength="50" required>
+                    <input type="number" placeholder="Numero" id="numero" name="txtNumero" value="" class="campomedio" oninput="limitarNumero(this)" min="0" required>
+                    <input type="text" placeholder="Cidade" id="cidade" name="txtCidade" value="" class="campomedio" maxlength="50" required>
+                    <input type="text" placeholder="Estado" id="estado" name="txtEstado" value="" class="campomedio" maxlength="2" required>
 
-                </div>
+                    <input type="email" name="email" id="email" value="" placeholder="E-mail" maxlength="100" class="campocheio" required>
 
-                <div class="conteudocaixarange">
-                    <div>
-                        <p id="textocaixarange">Tenho interesse em destinos:</p>
-                    </div>
+                    <input type="password" name="txtSenha" value="" id="senha" placeholder="Senha" class="campocheio" maxlength="20" required>
 
-                    <div class="moverjustifydestinos">
-                        <div class="justifydestinos">
-                            <div><input type="radio" name="destinos" value="N" <?php echo ($Inter_Nacional === 'N') ? 'checked' : ''; ?>>Nacionais</div>
-                            <div><input type="radio" name="destinos" value="I" <?php echo ($Inter_Nacional === 'I') ? 'checked' : ''; ?>>Internacionais</div>
-                            <div><input type="radio" name="destinos" value="A" <?php echo ($Inter_Nacional === 'A') ? 'checked' : ''; ?>>Ambos</div>
-                        </div>
-                    </div>
-
-                    <div class="faixapreco">
-                        <p>Em qual faixa de preço diário?</p>
-                    </div>
-
-                    <div class="alinharelementorange">
-                        <input type="range" id="valor" name="numberFaixa_Preco" min="0" max="10000" step="10" value="<?php echo $Faixa_preco ?>" oninput="mostrarValorIntervalo()">
-
-                    </div>
-                    <p id="valorOutput">R$ <?php echo $Faixa_preco ?></p>
                 </div>
 
                 <div id="botoes">
@@ -110,7 +101,7 @@
 
             </form>
 
-            <p id="folhascopy"><a href="../../">&copy;123folhas</a></p>
+            <p id="folhascopy"><a href="../../">&copy;Kabo</a></p>
         </section>
 
 
@@ -179,12 +170,6 @@
 
         function maskCEP(cep) {
             return cep.trim().replace(/^(\d{5})(\d{3})$/, '$1-$2')
-        }
-
-
-        function mostrarValorIntervalo() {
-            var valor = document.getElementById('valor').value;
-            document.getElementById('valorOutput').textContent = 'R$ ' + valor;
         }
 
         document.getElementById("excluirsubmit").addEventListener("click", function() {
