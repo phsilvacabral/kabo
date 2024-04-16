06b2e43302ec6016586b800f2c9e29cd223fbc91
Daniel Musse <127629791+iYoNuttxD@users.noreply.github.com>
2024-04-16 10:46:26 -0300

    Criptografia de senha

--- admin/login/login_php.php
+++ admin/login/login_php.php
@@ -3,12 +3,13 @@
 
     $login = $_POST["txtLogin"];
     $password = $_POST["txtPassword"];
+    $senhaCriptografada = md5($password);
 
     $sql = "SELECT Cod_Usuario, Senha, Tipo_Usuario FROM Usuario WHERE Email = '$login'";
     $result = $conn->query($sql);
     if ($result->num_rows > 0) {
         while($row = $result->fetch_assoc()) {
-            if ($row["Senha"] == $password) {
+            if ($row["Senha"] == $senhaCriptografada) {
                 session_start();
                 $_SESSION["Cod_Usuario"] = $row["Cod_Usuario"];
                 $_SESSION["Tipo_Usuario"] = $row["Tipo_Usuario"];
