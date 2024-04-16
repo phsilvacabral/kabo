0384343b6a8026cbe15fe29a79c6c28fb82938a0
Daniel Musse <127629791+iYoNuttxD@users.noreply.github.com>
2024-04-16 18:21:54 -0300

    Cadastro de placa mãe

--- admin/produtos/cadastrar/cadastro_php.php
+++ admin/produtos/cadastrar/cadastro_php.php
@@ -57,8 +57,48 @@
         }
     } else if ($tipo_cat == "GPU") {
 
-    } else if ($tipo_cat == "Placa_Mae") {
+    } else if ($tipo_cat == "PlacaMae") {
+        $Descricao = $_POST['descricaoPM'];
+        $Modelo = $_POST['modeloPM'];
+        $Marca = $_POST['marcaPM'];
+        $Preco = $_POST['precoPM'];
+        $Quantidade = $_POST['quantidadePM'];
+        $Tamanho = $_POST['tamanhoPM'];
+        $Soquete = $_POST['soquetePM'];
+        $Chipset = $_POST['chipsetPM'];
+        $Tipo_mem = $_POST['tipo_memPM'];
+        $Vel_mem = $_POST['vel_memPM'];
+        $PCIe = $_POST['PCIePM'];
+        $M2 = $_POST['M2PM'];
+        $SATA = $_POST['sataPM'];
 
+
+        try {
+            $checkQuery = "SELECT * FROM Produto_Tipo WHERE Modelo = '$Modelo'";
+            $checkResult = $conn->query($checkQuery);
+            if ($checkResult && $checkResult->num_rows > 0) {
+                throw new Exception('Modelo de produto já existente!');
+            } else {
+                $sql = "INSERT INTO Placa_Mae (Soquete, Tipo_Mem, Vel_Mem, PCIe, M2, SATA, Tamanho, Chipset) VALUES ('$Soquete', '$Tipo_mem', $Vel_mem, $PCIe, $M2, $SATA, '$Tamanho', '$Chipset')";
+                if ($conn->query($sql) === TRUE) {
+                    $sqlCod = "SELECT Cod_PlacaMae FROM Placa_Mae WHERE Soquete = '$Soquete' AND PCIe >= $PCIe AND Tipo_Mem = '$Tipo_mem' AND Vel_Mem = $Vel_mem AND M2 = $M2 AND SATA = $SATA AND Tamanho = '$Tamanho' AND Chipset = '$Chipset'";
+                    $resultCod = $conn->query($sqlCod);
+                    $row = $resultCod->fetch_assoc();
+                    $sqlP = "INSERT INTO Produto_Tipo (Descricao, Preco, Modelo, Marca, Qtd_estoque, fk_Cod_PlacaMae) VALUES ('$Descricao', $Preco, '$Modelo', '$Marca', $Quantidade, {$row['Cod_PlacaMae']})";
+                    if ($conn->query($sqlP) === TRUE) {
+                        header("Location: /kabo/admin/produtos");
+                        exit;
+                    } else {
+                        throw new Exception('Ocorreu um erro ao executar a operação.');
+                    }
+                } else {
+                    throw new Exception('Ocorreu um erro ao executar a operação.');
+                }
+            }
+        } catch (Exception $e) {
+            echo '<script>alert("'.$e->getMessage().'"); history.go(-1);</script>';
+            exit;
+        }
     }
 
 ?>
