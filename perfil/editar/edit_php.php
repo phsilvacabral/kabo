afbd291824cae72179823a3a1e5cf21f7a2ec889
Daniel Musse <127629791+iYoNuttxD@users.noreply.github.com>
2024-04-19 10:40:13 -0300

    Mudanças no envio da senha

--- perfil/editar/edit_php.php
+++ perfil/editar/edit_php.php
@@ -14,27 +14,47 @@
     $Cidade = $_POST['txtCidade'];
     $Estado = $_POST['txtEstado'];
 
-
-
-    try {
-        $sql = "UPDATE Usuario SET Nome = '$Nome', Genero = '$Genero', Senha = '$senhaCriptografada' WHERE Cod_Usuario = {$_SESSION['Cod_Usuario']}";
-        if ($conn->query($sql) === TRUE) {
-            $sqlC = "SELECT Cod_Usuario FROM Usuario WHERE CPF = '$CPF'";
-            $resultC = $conn->query($sqlC);
-            $row = $resultC->fetch_assoc();
-            $sqlE = "UPDATE Endereco SET CEP = '$CEP', Logradouro = '$Logradouro', Bairro = '$Bairro', Cidade = '$Cidade', Estado = '$Estado', Numero = $Numero WHERE fk_Cod_Usuario = {$_SESSION['Cod_Usuario']}";
-            if ($conn->query($sqlE) === TRUE) {
-                header("Location: /kabo/perfil");
-                exit;
+    if (empty($Senha)){
+        try {
+            $sql = "UPDATE Usuario SET Nome = '$Nome', Genero = '$Genero' WHERE Cod_Usuario = {$_SESSION['Cod_Usuario']}";
+            if ($conn->query($sql) === TRUE) {
+                $sqlC = "SELECT Cod_Usuario FROM Usuario WHERE CPF = '$CPF'";
+                $resultC = $conn->query($sqlC);
+                $row = $resultC->fetch_assoc();
+                $sqlE = "UPDATE Endereco SET CEP = '$CEP', Logradouro = '$Logradouro', Bairro = '$Bairro', Cidade = '$Cidade', Estado = '$Estado', Numero = $Numero WHERE fk_Cod_Usuario = {$_SESSION['Cod_Usuario']}";
+                if ($conn->query($sqlE) === TRUE) {
+                    header("Location: /kabo/perfil");
+                    exit;
+                } else {
+                    throw new Exception('Ocorreu um erro ao executar a operação.');
+                }
             } else {
                 throw new Exception('Ocorreu um erro ao executar a operação.');
             }
-        } else {
-            throw new Exception('Ocorreu um erro ao executar a operação.');
+        } catch (Exception $e) {
+            echo '<script>alert("'.$e->getMessage().'"); history.go(-1);</script>';
+            exit;
         }
-    } catch (Exception $e) {
-        echo '<script>alert("'.$e->getMessage().'"); history.go(-1);</script>';
-        exit;
+    } else {
+        try {
+            $sql = "UPDATE Usuario SET Nome = '$Nome', Genero = '$Genero', Senha = '$senhaCriptografada' WHERE Cod_Usuario = {$_SESSION['Cod_Usuario']}";
+            if ($conn->query($sql) === TRUE) {
+                $sqlC = "SELECT Cod_Usuario FROM Usuario WHERE CPF = '$CPF'";
+                $resultC = $conn->query($sqlC);
+                $row = $resultC->fetch_assoc();
+                $sqlE = "UPDATE Endereco SET CEP = '$CEP', Logradouro = '$Logradouro', Bairro = '$Bairro', Cidade = '$Cidade', Estado = '$Estado', Numero = $Numero WHERE fk_Cod_Usuario = {$_SESSION['Cod_Usuario']}";
+                if ($conn->query($sqlE) === TRUE) {
+                    header("Location: /kabo/perfil");
+                    exit;
+                } else {
+                    throw new Exception('Ocorreu um erro ao executar a operação.');
+                }
+            } else {
+                throw new Exception('Ocorreu um erro ao executar a operação.');
+            }
+        } catch (Exception $e) {
+            echo '<script>alert("'.$e->getMessage().'"); history.go(-1);</script>';
+            exit;
+        }
     }
-    
 ?>
