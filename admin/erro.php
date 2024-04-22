6dc5618b626a3d6db7c27ee595be8a488da7f16f
JoaoVitorHernandes <149633314+JoaoVitorHernandes@users.noreply.github.com>
2024-04-22 14:06:29 -0300

    Add files via upload

--- admin/erro.php
+++ admin/erro.php
@@ -1,73 +1,83 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
-    <title>Sem permissão</title>
-    <link rel="shortcut icon" href="../../../img/icon.png" type="image/x-icon">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="../img/icon.png" type="image/x-icon">
+    <link rel="stylesheet" href="style.css">
+    <title>Erro</title>
+    <style>
+        /*error*/
+        .sectionpgerror {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 90vh;
+        }
+
+        .backgroundboxerror {
+            background-color: #1E1E1E;
+            width: 580px;
+            max-width: 80%;
+            height: 380px;
+            box-shadow: 0px 0px 250px 20px #663234, 0px 0px 20px 0px #663234 inset;
+            border: 1px solid #F03333;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            padding: 20px;
+        }
+
+        .backgroundboxerror img {
+            width: 100px;
+            height: 45px;
+            margin-top: 48px;
+        }
+
+        #escritapermissao{
+            color: #FFFFFF;
+            margin-top: 70px;
+            font-size: 19px;
+            font-family: "poppins_bold";
+        }
+
+        .home_button {
+            background-color: #A73437BF;
+            width: 130px;
+            height: 40px;
+            border: 1px solid #F03333;
+            margin-top: 50px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            color: #FFFFFF;
+            font-family: "poppins_medio";
+            font-size: 18px;
+        }
+
+        .home_button:link {
+            text-decoration: none;
+        }
+        
+        #copyright{
+            color: #663234;
+            margin-top: 40px;
+            font-size: 12px;
+            font-family: "poppins_medio";
+        }
+    </style>
 </head>
 
-<style>
-    @font-face {
-        font-family: 'poppins';
-        src: url('../../../fonts/poppins/Poppins-Regular.ttf') format('truetype');
-        font-weight: normal;
-        font-style: normal;
-    }
-
-    @font-face {
-        font-family: 'poppins_medio';
-        src: url('../../../fonts/poppins/Poppins-Medium.ttf') format('truetype');
-        font-weight: normal;
-        font-style: normal;
-    }
-
-    body {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        background-color: #527265;
-    }
-
-    div {
-        max-width: 80%;
-        height: 50%;
-        color: #283028;
-        background-color: rgba(255, 255, 255, 0.75);
-        margin: 20px;
-        border-radius: 10px;
-    }
-    h1{
-        font-size: 30px;
-        font-family: 'poppins_medio';
-        margin: 20px;
-    }
-
-    a {
-        font-family: 'poppins';
-        font-size: 15px;
-        margin: 20px;
-        height: 30px;
-        width: 80px;
-        text-decoration: none;
-        color: white;
-        margin: 20px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        width: 90%;
-        background-color: #527265;
-        color: white;
-        border-radius: 5px;
-    }
-</style>
-
 <body>
-    <div>
-        <h1>Você não tem permissão para acessar esta área.</h1>
-        <a href="../../../">Voltar</a>
-    </div>
-
+    <section class="sectionpgerror">
+        <div class="backgroundboxerror">
+            <img src="../img/logo_neon.png" alt="">
+            <p id="escritapermissao">Você não tem permissão para acessar essa área!!</p>
+            <a href="../" class="home_button">Home</a>
+            <p id="copyright">&copy; Kabo </p>
+        </div>
+    </section>
 </body>
 
 </html>
