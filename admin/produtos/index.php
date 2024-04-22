b306df9de07f9d15868c48fb28098f615324612a
Pedro Cabral <126737207+phsilvacabral@users.noreply.github.com>
2024-04-22 10:28:06 -0300

    link icone e botao voltar

--- admin/produtos/index.php
+++ admin/produtos/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="../../img/icon.png" type="image/x-icon">
     <link rel="stylesheet" href="../style.css">
     <title>Produtos</title>
 </head>
@@ -123,10 +123,6 @@
             }
         });
 
-        document.getElementById('voltar').addEventListener('click', function(e) {
-    e.preventDefault();
-    history.back();
-});
     </script>
 </body>
 </html>
