8a5487b9628e30a1c46fa2823e9b76c25ea813d1
Pedro Cabral <126737207+phsilvacabral@users.noreply.github.com>
2024-05-19 01:43:05 -0300

    carrinho 100%

--- carrinho/carrinho.php
+++ carrinho/carrinho.php
@@ -85,9 +85,9 @@
                             </div>
                         </div>
                         <div class="divPrecoRemover">
-                            <p class="precoInvisivel" style="display: none; height: 0px"><?php echo $preco;?></p>
+                            <p class="precoInvisivel" style="display: none; height: 0px"><?php echo $preco; ?></p>
                             <p class="precoItem">R$ </p>
-                            <p class="parcelamentoCesta">10 x  sem juros no cartão</p>
+                            <p class="parcelamentoCesta">10 x sem juros no cartão</p>
                             <p class="removerItem">Remover</p>
                         </div>
                     </div><?php } ?>
@@ -119,7 +119,6 @@
                     } else {
                         echo "Nenhum endereço encontrado";
                     }
-                    $conn->close();
                     ?>
                     <a href="../perfil/editar/" id="editarEndereco">Editar</a>
                 </div>
@@ -152,11 +151,11 @@
                 <div id="divCupom" class="divsResumo">
                     <p id="textoCupom" class="textosResumo">Cupom de desconto: </p>
                     <span id="maisCupom">+</span>
-
                     <form action="" id="formCupom">
                         <input type="text" name="cupom" id="cupom" placeholder="Adicionar cupom">
                         <button id="botaoCupom">Aplicar</button>
                     </form>
+                    <span id="cupomUsado"></span>
                 </div>
 
                 <div id="divFreteResumo" class="divsResumo">
@@ -167,9 +166,18 @@
                 <div id="divCartaoPagamento" class="divsResumo">
                     <p id="textoCartao" class="textosResumo">Cartão de pagamento: </p>
                     <div id="selectCartoes">
+                        <?php
+                        $sqlCartao = "SELECT Cod_Cartao, Numero FROM Cartao_Pagamento WHERE fk_Cod_Usuario = ? ORDER BY Cod_Cartao ASC";
+                        $stmtC = $conn->prepare($sqlCartao);
+                        $stmtC->bind_param('i', $Cod_Usuario);
+                        $stmtC->execute();
+                        $resultC = $stmtC->get_result();
+                        ?>
                         <select name="cartao" id="selectCartao">
-                            <option value="">Cartão 1</option>
-                            <option value="">Cadastrar novo cartão</option>
+                            <?php while ($row = $resultC->fetch_assoc()) {
+                                echo '<option value="' . $row['Numero'] . '">Cartão ****' . substr($row['Numero'], -4) . '</option>';
+                            } ?>
+                            <option value="novo">Cadastrar novo cartão</option>
                         </select>
                     </div>
                 </div>
@@ -192,223 +200,25 @@
 
                 <div id="divValorTotal">
                     <p id="textoValorTotal" class="textosResumo">Valor total: </p>
-                    <p id="valorTotalFinal">R$ 3.999,00</p>
-                    <p id="textParceFinalizar">10 x 190,00 sem juros no cartão de crédito</p>
+                    <p id="valorTotalFinal">R$ 0,00</p>
+                    <p id="textParceFinalizar"></p>
                 </div>
 
-                <div id="botaoFinalizarCompra"><img src="../img/cestaBranca.png" alt=""> Finalizar compra</div>
+                <form action="">
+                    <input id="descontoAplicado" type="hidden" name="desconto" value="">
+                    <input id="codCupom" type="hidden" name="codCupom" value="">
+                    <input id="valorCompra" type="hidden" name="valorTotal" value="">
+                    <input id="cartaoSelecionado" type="hidden" name="cartaoSelecionado" value="">
+                    <div id="botaoFinalizarCompra"><img src="../img/cestaBranca.png" alt=""> Finalizar compra</div>
+                </form>
+
             </div>
         </div>
     </main>
 
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script>
-        /* função voltar */
-        function voltar() {
-            window.history.back();
-        }
-
-
-        /* função para aparecer divEntregaResumo quando clicar em continuar */
-        $(document).ready(function() {
-            $('#botaoFinalizarCesta').click(function() {
-                var parentWidth = $('#divCestaProdutos').parent().width();
-                var newCestaWidth = parentWidth * 0.666;
-                var newResumoWidth = parentWidth * 0.333;
-                var buttonHeight = $(this).outerHeight(true);
-                var currentCestaHeight = $('#divCestaProdutos').outerHeight();
-                var newCestaHeight = currentCestaHeight - buttonHeight;
-
-                $('#divCestaProdutos').animate({
-                    width: newCestaWidth,
-                    height: newCestaHeight
-                }, 650);
-
-                $('#divEntregaResumo').css({
-                    display: 'block',
-                    width: '0px'
-                }).animate({
-                    width: newResumoWidth + 'px'
-                }, 0);
-
-                $(this).fadeOut('fast');
-            });
-        });
-
-
-        /* função para aparecer adicionar cupom */
-        document.getElementById('maisCupom').addEventListener('click', function() {
-            var formCupom = document.getElementById('formCupom');
-            var maisCupom = document.getElementById('maisCupom');
-            if (formCupom.style.display === 'none' || formCupom.style.display === '') {
-                $(formCupom).fadeIn(200);
-                formCupom.style.display = 'flex';
-                $(maisCupom).fadeOut(0);
-            } else {
-                $(formCupom).fadeOut('fast');
-                $(maisCupom).fadeIn('fast');
-            }
-        });
-
-
-        /* crud carrinho */
-        $(document).ready(function() {
-            function atualizarSoma() {
-                var soma = 0;
-                $('.number-quantity').each(function() {
-                    soma += parseInt($(this).val());
-                });
-                $('#qtdCarrinho').text(soma + ' itens');
-            }
-        
-            $('.number-quantity').change(function(e) {
-                e.preventDefault();
-                var form = $(this).closest('form');
-                $.ajax({
-                    url: 'quantidadeCarrinho.php',
-                    type: 'post',
-                    data: form.serialize(),
-                    success: function(response) {
-                        console.log(response);
-                    }
-                });
-        
-                if ($(this).val() == 0) {
-                    $(this).closest('.itemCesta').fadeOut(200);
-                }
-        
-                atualizarSoma();
-            });
-        
-            $('.removerItem').click(function() {
-                $(this).closest('.itemCesta').find('.number-quantity').val(0).trigger('change');
-            });
-        
-            atualizarSoma();
-        });
-
-
-        /* função pra aumentar a quantidade do item */
-        $(document).ready(function() {
-            $('.number-right').click(function() {
-                var input = $(this).siblings('.number-quantity');
-                var valorAtual = parseInt(input.val());
-                var valorMaximo = parseInt(input.attr('max'));
-
-                if (valorAtual < valorMaximo) {
-                    input.val(valorAtual + 1).trigger('change');
-                }
-
-                if (valorAtual + 1 >= valorMaximo) {
-                    $(this).css('display', 'none');
-                }
-            });
-        });
-
-
-        /* função para diminuir a quantidade do item */
-        $('.number-left').click(function() {
-            var input = $(this).siblings('.number-quantity');
-            var valorAtual = parseInt(input.val());
-            var valorMinimo = parseInt(input.attr('min'));
-
-            if (valorAtual > valorMinimo) {
-                input.val(valorAtual - 1).trigger('change');
-            }
-
-            if (valorAtual - 1 < parseInt(input.attr('max'))) {
-                $(this).siblings('.number-right').css('display', 'block');
-            }
-        });
-
-
-        /* função para atualizar subtotal de acordo com o preço dos itens */
-        $(document).ready(function() {
-            function atualizarSubtotal() {
-                var subtotal = 0;
-                $('.precoItem').each(function() {
-                    var precoItemText = $(this).text().replace('R$', '').replace(',', '.');
-                    var precoItem = parseFloat(precoItemText);
-                    if (!isNaN(precoItem)) {
-                        subtotal += precoItem;
-                    }
-                });
-                $('#valorSubtotal').text('R$ ' + subtotal.toFixed(2).replace('.', ','));
-            }
-        
-            /* função para atualizar o preço dos itens */
-            $(document).ready(function() {
-            $('.number-quantity').on('change keyup', function() {
-                var quantidade = parseInt($(this).val());
-                var precoUnitarioText = $(this).closest('.itemCesta').find('.precoInvisivel').text().replace('R$', '').replace(',', '.');
-                var precoUnitario = parseFloat(precoUnitarioText);
-                if (!isNaN(quantidade) && !isNaN(precoUnitario)) {
-                    var precoTotal = (precoUnitario * quantidade).toFixed(2);
-                    $(this).closest('.itemCesta').find('.precoItem').text('R$ ' + precoTotal.replace('.', ','));
-                    var parcelamento = (precoTotal / 10).toFixed(2);
-                    $(this).closest('.itemCesta').find('.parcelamentoCesta').text('10 x R$ ' + parcelamento.replace('.', ',') + ' sem juros no cartão');
-                }
-                atualizarSubtotal();
-            }).trigger('change');
-        });
-        
-            /* função para esvaziar carrinho */
-            $('#esvaziarCarrinho').click(function() {
-                $('.number-quantity').val(0).trigger('change');
-                setTimeout(function() {
-                    window.location.href = "../carrinho/";
-                }, 200);
-            });
-        
-            /* função para remover item individual */
-            $('.removerItem').click(function() {
-                $(this).closest('.itemCesta').find('.number-quantity').val(0).trigger('change');
-                var existeItem = false;
-                $('.number-quantity').each(function() {
-                    if (parseInt($(this).val()) > 0) {
-                        existeItem = true;
-                        return false;
-                    }
-                });
-                if (!existeItem) {
-                    setTimeout(function() {
-                        window.location.href = "../carrinho/";
-                    }, 200);
-                }
-            });
-        
-            // Atualiza o subtotal quando a página é carregada
-            atualizarSubtotal();
-        });
-
-
-        /* função para adicionar cupom de desconto */
-        $(document).ready(function() {
-            $('#form').submit(function(e) {
-                e.preventDefault();
-                var dadosFormulario = $(this).serialize();
-                $.ajax({
-                    type: 'POST',
-                    url: 'addCupom.php',
-                    data: dadosFormulario,
-                    success: function(response) {
-                        var desconto = parseFloat(response);
-                        if (desconto > 0) {
-                            var valorTotalFinalText = $('#valorTotalFinal').text().replace('R$', '').replace(',', '.');
-                            var valorTotalFinal = parseFloat(valorTotalFinalText);
-                            if (!isNaN(valorTotalFinal)) {
-                                var valorComDesconto = valorTotalFinal - (valorTotalFinal * (desconto / 100));
-                                $('#valorTotalFinal').text('R$ ' + valorComDesconto.toFixed(2).replace('.', ','));
-                            }
-                        } else {
-                            // Código para lidar com a situação em que o cupom não é válido
-                        }
-                    }
-                });
-            });
-        });
-    </script>
+    <script src="script.js"></script>
 
 
 </body>
